6fac56f43ac25160cc8ddd7923290e8cec3b63d6
dafg05 <danialefloresg@gmail.com>
2024-01-19 12:24:18 -0500

    synthesize audio

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='midiUtils',
-    version='0.1.5',
+    version='0.1.6',
     author='Daniel Flores Garcia',
     author_email='danialefloresg@gmail.com',
     description='Used for drum gen project.',
