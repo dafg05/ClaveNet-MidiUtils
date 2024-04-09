85e8cc4d2aea3a49842576966eb01f49f146137f
dafg05 <danialefloresg@gmail.com>
2024-04-09 00:25:34 -0400

    up version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='midiUtils',
-    version='0.2.3',
+    version='0.2.4',
     author='Daniel Flores Garcia',
     author_email='danialefloresg@gmail.com',
     description='Used for drum gen project.',
